36aea1a2eb5133e50a81d5a384d4891ad3074a83
tsai97216 <123880738+tsai97216@users.noreply.github.com>
2026-06-12 21:32:51 +0800

    Update update_source.py

--- update_source.py
+++ update_source.py
@@ -35,7 +35,7 @@ APPS = [
         "repo": "Mark02-2012/YTPlusM",
         "name": "YTPlusM",
         "bundleID": "com.mark.ytplusm",
-        "icon": "https://raw.githubusercontent.com/Mark02-2012/YTPlusM/blob/main/Resources/IMG_5913.png",
+        "icon": "https://raw.githubusercontent.com/Mark02-2012/YTPlusM/main/Resources/IMG_5913.png",
         "subtitle": "YouTube 修改版",
         "desc": "提供去廣告、播放優化與額外功能。"
     }
